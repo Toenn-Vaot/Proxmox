d54d45d9d09ac0690d4593fba2d7339e02af221b
Yoann BLOSSIER <60755917+yblossier@users.noreply.github.com>
2025-11-22 06:03:13 +0100

    Update source URL in n8n.sh script
    Signed-off-by: Yoann BLOSSIER <60755917+yblossier@users.noreply.github.com>

--- scripts/n8n/n8n.sh
+++ scripts/n8n/n8n.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-source <(curl -s https://raw.githubusercontent.com/Toenn-Vaot/Proxmox/main/misc/build.func)
+source <(curl -s https://raw.githubusercontent.com/Toenn-Vaot/Proxmox/main/scripts/misc/build.func)
 # Copyright (c) 2021-2024 tteck
 # Author: tteck (tteckster)
 # License: MIT
